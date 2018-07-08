e34925ec870927353947e8e2adbb056768825e5e
Ḿoritz Bösenberg <morit_z@hotmail.de>
2018-07-08 18:51:54 +0200

    First push for Decoder Project -> Code

---
+++ STM32F103/Debug/Src/subdir.mk
@@ -0,0 +1,43 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/gpio.c \
+../Src/main.c \
+../Src/stm32f1xx_hal_msp.c \
+../Src/stm32f1xx_it.c \
+../Src/system_stm32f1xx.c \
+../Src/tim.c \
+../Src/usart.c 
+
+OBJS += \
+./Src/gpio.o \
+./Src/main.o \
+./Src/stm32f1xx_hal_msp.o \
+./Src/stm32f1xx_it.o \
+./Src/system_stm32f1xx.o \
+./Src/tim.o \
+./Src/usart.o 
+
+C_DEPS += \
+./Src/gpio.d \
+./Src/main.d \
+./Src/stm32f1xx_hal_msp.d \
+./Src/stm32f1xx_it.d \
+./Src/system_stm32f1xx.d \
+./Src/tim.d \
+./Src/usart.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o: ../Src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -mfloat-abi=soft '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F103xB -I"C:/Users/Morit/Desktop/_STMspielwiese/STM32F103/Inc" -I"C:/Users/Morit/Desktop/_STMspielwiese/STM32F103/Drivers/STM32F1xx_HAL_Driver/Inc" -I"C:/Users/Morit/Desktop/_STMspielwiese/STM32F103/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy" -I"C:/Users/Morit/Desktop/_STMspielwiese/STM32F103/Drivers/CMSIS/Device/ST/STM32F1xx/Include" -I"C:/Users/Morit/Desktop/_STMspielwiese/STM32F103/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
